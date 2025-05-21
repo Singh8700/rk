ca8285c46c62ba00daa5034162650ffbcc3451e5
Rohit <101079442+Singh8700@users.noreply.github.com>
2025-05-21 15:43:55 +0530

    sitemap add

---
+++ next-sitemap.config.js
@@ -0,0 +1,13 @@
+// next-sitemap.config.js
+module.exports = {
+    siteUrl: 'https://rk960.github.io/rk960/', // 🔁 अपनी site का URL डालें
+    generateRobotsTxt: true, // robots.txt भी generate करेगा
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 7000,
+    exclude: ['/admin/*', '/secret-page'],
+    robotsTxtOptions: {
+      policies: [{ userAgent: '*', allow: '/' }],
+    },
+  };
+  
